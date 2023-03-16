4d50b0bf23aad0a8e02b7f5998a906e2052d4870
heyvinnnny <kevinyew97@hotmail.com>
2023-03-16 22:08:52 +0800

    Update connection string

--- server/index.js
+++ server/index.js
@@ -27,8 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //const client = new MongoClient('mongodb+srv://bt4301group1:bt4301@mlops.mxl46jm.mongodb.net/mlops?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://bt4301group1:bt4301@mlops.mxl46jm.mongodb.net/mlops?retryWrites=true&w=majority', {
+//MongoDB Atlas URI: mongodb+srv://bt4301group1:bt4301@mlops.mxl46jm.mongodb.net/mlops?retryWrites=true&w=majority
+// Connect to MongoDB Atlas 
+//Local MongoDB URI: mongodb://localhost:27017/mlops
+//Need to install MongoDB first
+mongoose.connect('mongodb://localhost:27017/mlops', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
@@ -197,52 +200,54 @@ app.post('/register', (req, res, next) => {
 // });
 
 //forget password
-app.post('/forgotpassword', async (req, res) => {
-  try {
-    const { email } = req.body;
+// app.post('/forgotpassword', async (req, res) => {
+//   try {
+//     const { email } = req.body;
 
-    // Check if the email address provided in the request body exists in your MongoDB database.
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+//     // Check if the email address provided in the request body exists in your MongoDB database.
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(404).json({ message: 'User not found' });
+//     }
 
-    // Generate a unique token for the password reset request.
-    const token = crypto.randomBytes(20).toString('hex');
+//     // Generate a unique token for the password reset request.
+//     const token = crypto.randomBytes(20).toString('hex');
 
-    // Insert a new document in the password-reset-tokens collection with the email address and token.
-    const resetToken = new PasswordResetToken({
-      email,
-      token
-    });
-    await resetToken.save();
+//     // Insert a new document in the password-reset-tokens collection with the email address and token.
+//     const resetToken = new PasswordResetToken({
+//       email,
+//       token
+//     });
+//     await resetToken.save();
 
-    // Send an email to the user with a link to a password reset page.
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
+//     // Send an email to the user with a link to a password reset page.
+//     const transporter = nodemailer.createTransport({
+//       service: 'Gmail',
+//       auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD
+//       }
+//     });
 
-    const resetUrl = `${req.protocol}://${req.hostname}/reset-password?token=${token}`;
+//     const resetUrl = `${req.protocol}://${req.hostname}/reset-password?token=${token}`;
 
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: 'Reset Your Password',
-      text: `Hi ${user.name},\n\nYou are receiving this email because you (or someone else) has requested to reset the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\n${resetUrl}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
-    };
+//     const mailOptions = {
+//       from: process.env.EMAIL_USERNAME,
+//       to: email,
+//       subject: 'Reset Your Password',
+//       text: `Hi ${user.name},\n\nYou are receiving this email because you (or someone else) has requested to reset the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\n${resetUrl}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
+//     };
 
-    await transporter.sendMail(mailOptions);
+//     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ message: 'Password reset link sent' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+//     res.status(200).json({ message: 'Password reset link sent' });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// });
+
+//forget password 2
 
 // Start the server
 const PORT = process.env.PORT || 3000;
