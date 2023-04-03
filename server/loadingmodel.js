fe877c5d4400b172ddfbb5554721eb538d140010
zhenging2b <zhenging2b@gmail.com>
2023-04-03 14:56:28 +0800

    Added upload code page
    Added upload code page

--- server/loadingmodel.js
+++ server/loadingmodel.js
@@ -1,15 +1,8 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs')
 
-
-
-const modelUrl = 'file:///tmp/my-model-1';
-
-let model;
-
-// load COCO-SSD graph model from TensorFlow Hub
+// load  model from local folder 
 const loadModel = async function () {
-  console.log(`loading model from ${modelUrl}`);
   const model = await tf.loadLayersModel('file://mlModel/model.json');
 
   const prediction = model.predict(tf.randomNormal([3, 784]));
