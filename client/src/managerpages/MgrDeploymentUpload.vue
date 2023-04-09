71164951dd288c0df31624ba334e237f5d17daf1
Avery Ong <yienong0309@gmail.com>
2023-04-10 00:35:28 +0800

    Added deployment options - blue/green & canary

--- client/src/managerpages/MgrDeploymentUpload.vue
+++ client/src/managerpages/MgrDeploymentUpload.vue
@@ -4,15 +4,33 @@
     <form>
       <div class="form-group">
         <label for="deployment-id">Deployment ID:</label>
-        <input type="text" class="form-control" id="deployment-id" v-model="deploymentId" pattern="[0-9]+" required>
+        <input
+          type="text"
+          class="form-control"
+          id="deployment-id"
+          v-model="deploymentId"
+          pattern="[0-9]+"
+          required
+        />
       </div>
       <div class="form-group">
         <label for="deployment-name">Deployment Name:</label>
-        <input type="text" class="form-control" id="deployment-name" v-model="deploymentName" required>
+        <input
+          type="text"
+          class="form-control"
+          id="deployment-name"
+          v-model="deploymentName"
+          required
+        />
       </div>
       <div class="form-group">
         <label for="importance">Importance:</label>
-        <select class="form-control" id="importance" v-model="importance" required>
+        <select
+          class="form-control"
+          id="importance"
+          v-model="importance"
+          required
+        >
           <option value="">Choose an option</option>
           <option value="Low">Low</option>
           <option value="Medium">Medium</option>
@@ -22,15 +40,137 @@
       </div>
       <div class="form-group">
         <label for="date-now">Date Now:</label>
-        <input type="date" class="form-control" id="date-now" v-model="dateNow" required readonly>
+        <input
+          type="date"
+          class="form-control"
+          id="date-now"
+          v-model="dateNow"
+          required
+          readonly
+        />
       </div>
       <div class="form-group">
         <label for="model-version">Email:</label>
-        <input type="text" class="form-control" id="email-version" v-model="user.email" disabled required>
+        <input
+          type="text"
+          class="form-control"
+          id="email-version"
+          v-model="user.email"
+          disabled
+          required
+        />
       </div>
       <div class="form-group">
         <label for="model-version">Deployment Description:</label>
-        <input type="text" class="form-control" id="deployment-description" v-model="deployDescription" required>
+        <input
+          type="text"
+          class="form-control"
+          id="deployment-description"
+          v-model="deployDescription"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="deploymentType">Deployment Type:</label>
+        <br />
+        <input
+          type="radio"
+          id="canary"
+          value="Canary"
+          v-model="selectedDeployment"
+          @change="handleDeploymentSelection"
+        />
+        <label for="canary">Canary</label>
+        &nbsp;&nbsp;&nbsp;
+        <input
+          type="radio"
+          id="blueGreen"
+          value="Blue-Green"
+          v-model="selectedDeployment"
+          @change="handleDeploymentSelection"
+        />
+        <label for="blueGreen">Blue-Green</label>
+        <div class="form" v-if="showInputs">
+          <h4>Selected Deployment: {{ selectedDeployment }} Deployment.</h4>
+          <h5>Please Input IaC Features below:</h5>
+          <div v-if="selectedDeployment === 'Blue-Green'">
+            <!-- Add input fields for Blue/Green deployment IaC features -->
+            <div class="form-group">
+              <label for="active-environment">Active Environment:</label>
+              <br />
+              <input type="radio" id="blue" name="active-environment" />
+              <label for="blue">Blue</label>
+              &nbsp;&nbsp;&nbsp;
+              <input type="radio" id="green" name="active-environment" />
+              <label for="green">Green</label>
+            </div>
+            <div class="form-group">
+              <label for="switching-method">Switching Method:</label>
+              <select
+                class="form-control"
+                id="switching-method"
+                v-model="switching"
+                required
+              >
+                <option value="">Choose an option</option>
+                <option value="Service Health">DNS Switch</option>
+                <option value="Data Drift">Load Balancer</option>
+                <option value="Accuracy Rate">Accuracy Rate</option>
+                <option value="Critical">Service Mesh</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-5" v-else-if="selectedDeployment === 'Canary'">
+            <!-- Add input fields for Canary deployment IaC features -->
+            <div class="form-group">
+              <label for="traffic-percentage">Traffic Percentage (%):</label>
+              <input
+                type="text"
+                class="form-control"
+                id="traffic-percentage"
+                v-model="deployDescription"
+                required
+              />
+            </div>
+            <div class="form-group">
+              <label for="deployment-duration"
+                >Deployment Duration (days):</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="deployment-duration"
+                v-model="deployDescription"
+                required
+              />
+            </div>
+            <div class="form-group">
+              <label for="rollback-criteria">Rollback Criteria:</label>
+              <select
+                class="form-control"
+                id="rollback-criteria"
+                v-model="rollback"
+                required
+              >
+                <option value="">Choose an option</option>
+                <option value="Service Health">Service Health</option>
+                <option value="Data Drift">Data Drift</option>
+                <option value="Accuracy Rate">Accuracy Rate</option>
+                <option value="Critical">Critical</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="rollback-threshold">Rollback Threshold:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="rollback-threshold"
+                v-model="deployDescription"
+                required
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <!-- <div class="form-group">
         <label for="env-version">Environment Version:</label>
@@ -40,14 +180,18 @@
         <label for="replacement-reason">Replacement Reason:</label>
         <textarea class="form-control" id="replacement-reason" v-model="replacementReason" required></textarea>
       </div> -->
-      <button type="submit" class="btn btn-primary" @click.prevent="submitForm">Submit</button>
-      <div v-show="isSuccess" class="success-message">Deployment created successfully!</div>
+      <button type="submit" class="btn btn-primary" @click.prevent="submitForm">
+        Submit
+      </button>
+      <div v-show="isSuccess" class="success-message">
+        Deployment created successfully!
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   mounted() {
@@ -55,16 +199,19 @@ export default {
   },
   data() {
     return {
-      deploymentId: '',
-      deploymentName: '',
-      importance: '',
+      deploymentId: "",
+      deploymentName: "",
+      importance: "",
       dateNow: new Date().toISOString().slice(0, 10),
-      deployDescription: '',
+      deployDescription: "",
       //envVersion: '',
       //replacementReason: '',
-      email: "",
-      isSuccess: false
-    }
+
+      isSuccess: false,
+      selectedDeployment: "",
+      selectedRollback: "",
+      showInputs: false,
+    };
   },
   computed: {
     user: {
@@ -93,7 +240,7 @@ export default {
   methods: {
     async submitForm() {
       try {
-        const response = await axios.post('http://localhost:3000/deployments', {
+        const response = await axios.post("http://localhost:3000/deployments", {
           deploymentId: this.deploymentId,
           deploymentName: this.deploymentName,
           importance: this.importance,
@@ -108,18 +255,21 @@ export default {
         this.isSuccess = true;
 
         // Reset the form fields
-        this.deploymentId = '';
-        this.deploymentName = '';
-        this.importance = '';
+        this.deploymentId = "";
+        this.deploymentName = "";
+        this.importance = "";
         this.dateNow = new Date().toISOString().slice(0, 10);
-        this.deployDescription = '';
-        this.email = '';
+        this.deployDescription = "";
+        this.email = "";
       } catch (error) {
         console.error(error);
       }
     },
-  }
-}
+    handleDeploymentSelection() {
+      this.showInputs = true;
+    },
+  },
+};
 </script>
 
 <style scoped>
